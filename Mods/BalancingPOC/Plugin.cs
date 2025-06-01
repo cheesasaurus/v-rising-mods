7f37475ed8e100c19a0c7a2a2ab4f15a73fe630b
Nicholas Toby <cheesasaurus@gmail.com>
2025-06-01 18:32:10 -0500

    initd BalancingPOC project

---
+++ Mods/BalancingPOC/Plugin.cs
@@ -0,0 +1,54 @@
+﻿using BepInEx;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using VampireCommandFramework;
+using VRisingMods.Core.Utilities;
+
+namespace BalancingPOC;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+[BepInDependency("gg.deca.VampireCommandFramework")]
+public class Plugin : BasePlugin
+{
+    Harmony _harmony;
+
+    public override void Load()
+    {
+        // Plugin startup logic
+        LogUtil.Init(Log);
+        Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} version {MyPluginInfo.PLUGIN_VERSION} is loaded!");
+
+        // Harmony patching
+        _harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+        _harmony.PatchAll(System.Reflection.Assembly.GetExecutingAssembly());
+
+        // Register all commands in the assembly with VCF
+        CommandRegistry.RegisterAll();
+    }
+
+    public override bool Unload()
+    {
+        CommandRegistry.UnregisterAssembly();
+        _harmony?.UnpatchSelf();
+        return true;
+    }
+
+    // // Uncomment for example commmand or delete
+
+    // /// <summary> 
+    // /// Example VCF command that demonstrated default values and primitive types
+    // /// Visit https://github.com/decaprime/VampireCommandFramework for more info 
+    // /// </summary>
+    // /// <remarks>
+    // /// How you could call this command from chat:
+    // ///
+    // /// .balancingpoc-example "some quoted string" 1 1.5
+    // /// .balancingpoc-example boop 21232
+    // /// .balancingpoc-example boop-boop
+    // ///</remarks>
+    // [Command("balancingpoc-example", description: "Example command from balancingpoc", adminOnly: true)]
+    // public void ExampleCommand(ICommandContext ctx, string someString, int num = 5, float num2 = 1.5f)
+    // { 
+    //     ctx.Reply($"You passed in {someString} and {num} and {num2}");
+    // }
+}
---
+++ Mods/BalancingPOC/README.md
@@ -0,0 +1,3 @@
+# BalancingPOC
+
+⚠️WIP⚠️
